5d7a83b9fa7de2409eaafca055fa462b458a4c11
Erin Fisher <flybirdie85@gmail.com>
2020-10-14 18:36:32 -0700

    trying to debug

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,20 @@ import SearchBooks from "./pages/SearchBooks";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer"
-import "./App.css"
-
+import "./App.css";
 
 function App() {
+
   return (
     <Router>
       <div>
         <Nav />
-        <Switch>
+        <Switch> 
           <Route exact path="/" component={SearchBooks} />
           <Route exact path="/saved" component={SaveBooks} />
           <Route exact path="/saved/:id" component={SaveBooks} />
           <Route component={NoMatch} /> 
-        </Switch>
+        </Switch> 
         <Footer />
       </div>
     </Router>
